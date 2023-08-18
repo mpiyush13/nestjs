b78a83e01126ca859c2ed92f692fa8b0cd5942de
Piyush Maurya <piyushmaurya203@gmail.com>
2023-08-18 05:57:30 -0700

    crud opreation with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'',
+      database:'myDB',
+      autoLoadEntities:true,
+      synchronize:true,
+    })],
 })
 export class AppModule {}
